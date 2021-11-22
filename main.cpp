7bf22e9ca318e71e12145f33f25599749116a925
Mohammed Fahad F S <52351395+fahadpro01@users.noreply.github.com>
2021-11-22 14:35:21 +0000

    nil

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }   
+  int counter=1;
+  while(counter<n){
+    for(int i=0;i<n-counter;i++){
+      if(a[i]>a[i+1]){
+        int temp;
+        temp=a[i];
+        a[i]=a[i+1];
+        a[i+1]=temp;
+      }
+    } counter++;
+  }
+  for(int i=0;i<n;i++){
+    cout<<a[i]<<endl;
+  }
 } 
